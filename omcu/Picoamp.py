52399b70cccea19a7f63b3b028fbc59ad665933e
canada <canada@straw.com>
2021-04-22 17:07:56 +0200

    minor changes

--- omcu/Picoamp.py
+++ omcu/Picoamp.py
@@ -3,6 +3,7 @@ import serial
 import time
 import numpy as np
 
+
 class Picoamp:
     """
     This is a class for the Keithley 6482 Picoamperemeter
@@ -17,8 +18,8 @@ class Picoamp:
                                     )
         startup = ['*RST', 'SENS:CURR:RANG:AUTO ON', 'SENS2:CURR:RANG:AUTO ON', 'SYST:AZER ON', 'SYST:AZER OFF']
         for i in startup:
-            self.serial.write(str.encode('%s\r' %i))
-            #print('\t%s' %i)
+            self.serial.write(str.encode('%s\r' % i))
+            # print('\t%s' %i)
             time.sleep(1.0)
 
     def read_ch1(self, ncal):
@@ -31,27 +32,27 @@ class Picoamp:
         comms = []
         i = 1
         comms.append(str.encode('TRIG:DEL 0\n'))
-        comms.append(str.encode('ARM:COUN %s\n' %ncal))
+        comms.append(str.encode('ARM:COUN %s\n' % ncal))
         comms.append(str.encode('INIT\n'))
         comms.append(str.encode('READ?\n'))
-        for comm  in comms:
-            #print('\t' + comm[:-1].decode("utf-8") )
+        for comm in comms:
+            # print('\t' + comm[:-1].decode("utf-8") )
             self.serial.write(comm)
             time.sleep(.1)
         line = self.serial.readline()
-        #print(line)
+        # print(line)
         try:
             val = np.array([])
             for x in line.decode('utf8').split(',')[::2]:
-                val = np.append(val,[float(x)])
+                val = np.append(val, [float(x)])
         except:
             print("\tMeasurement failed, trying again...")
-            i +=1
-            if i >=50:
+            i += 1
+            if i >= 50:
                 return np.nan, np.nan
-        print('\tPhotodiode current Ch1: %s A' %val)
-        #mean = np.mean(val)
-        #std  = np.std(val) / np.sqrt(ncal)
+        print('\tPhotodiode current Ch1: %s A' % val)
+        # mean = np.mean(val)
+        # std  = np.std(val) / np.sqrt(ncal)
         return val
 
     def read_ch2(self, ncal):
@@ -64,29 +65,30 @@ class Picoamp:
         comms = []
         i = 1
         comms.append(str.encode('TRIG:DEL 0\n'))
-        comms.append(str.encode('ARM:COUN %s\n' %ncal))
+        comms.append(str.encode('ARM:COUN %s\n' % ncal))
         comms.append(str.encode('INIT\n'))
         comms.append(str.encode('READ?\n'))
-        for comm  in comms:
-            #print('\t' + comm[:-1].decode("utf-8") )
+        for comm in comms:
+            # print('\t' + comm[:-1].decode("utf-8") )
             self.serial.write(comm)
             time.sleep(.1)
         line = self.serial.readline()
-        #print(line)
+        # print(line)
         try:
             val = np.array([])
             for x in line.decode('utf8').split(',')[::2]:
-                val = np.append(val,[float(x)])
+                val = np.append(val, [float(x)])
         except:
             print("\tMeasurement failed, trying again...")
-            i +=1
-            if i >=50:
+            i += 1
+            if i >= 50:
                 return np.nan, np.nan
-        print('\tPhotodiode current Ch2: %s A' %val)
-        #mean = np.mean(val)
-        #std  = np.std(val) / np.sqrt(ncal)
+        print('\tPhotodiode current Ch2: %s A' % val)
+        # mean = np.mean(val)
+        # std  = np.std(val) / np.sqrt(ncal)
         return val
 
-P=Picoamp()
+
+P = Picoamp()
 P.read_ch1(10)
 P.read_ch2(10)
