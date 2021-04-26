d6692eed9b108f30f21a3dc680a0e964358d86cb
Laura <evalaura.winter@tum.de>
2021-04-26 15:12:20 +0200

    Powermeter echo Fkt

--- omcu/Powermeter.py
+++ omcu/Powermeter.py
@@ -15,13 +15,45 @@ class Powermeter:
                                     timeout=2
                                     )
 
+        self.echo(0)  # echo off
+        self.serial.write(str.encode('PM:L 405'))  # the Picosecond Laser has a wavelength of 405 nm
+
+    def echo(self, state):
+        """
+        This is a function to turn on or off the echoing of commands sent to the power meter.
+        When the echo mode is enabled, the power meter generates a '>' prompt for every new line and all characters
+        sent to the power meter are echoed back over the interface.
+        When the echo mode is disabled (normal mode) the power meter does not generate a prompt or echo character
+        back over the interface.
+        :param state: echo set (0 Echo OFF, 1 Echo ON)
+        :return: echo status
+        """
+        if state == 0:
+            self.serial.write(str.encode('ECHO 0'))  # Echo OFF
+            time.sleep(.1)
+            line = self.serial.readline()
+            print(line.decode())
+        else:
+            pass
+        if state == 1:
+            self.serial.write(str.encode('ECHO 1'))  # Echo ON
+            time.sleep(.1)
+            line = self.serial.readline()
+            print(line.decode())
+        else:
+            pass
+        self.serial.write(str.encode('ECHO?'))  # returns the status of the echo
+        time.sleep(.1)
+        line = self.serial.readline()
+        print("The Echo status is:", line.decode(), "(0 Echo OFF, 1 Echo ON)")
+
     def echo_on(self):
         """
         This is a function to turn on the echoing of commands sent to the power meter.
         When the echo mode is enabled, the power meter generates a '>' prompt for every new line and all characters
         sent to the power meter are echoed back over the interface.
         """
-        self.serial.write(str.encode('ECHO ON'))
+        self.serial.write(str.encode('ECHO 1'))  # Echo ON
         time.sleep(.1)
         line = self.serial.readline()
         print(line.decode())
@@ -36,7 +68,7 @@ class Powermeter:
         When the echo mode is disabled (normal mode) the power meter does not generate a prompt or echo character
         back over the interface.
         """
-        self.serial.write(str.encode('ECHO OFF'))
+        self.serial.write(str.encode('ECHO 0'))  # Echo OFF
         time.sleep(.1)
         line = self.serial.readline()
         print(line.decode())
