b35390a4fd9bf644645f574fc8776950aeba5a87
Laura <evalaura.winter@tum.de>
2021-05-04 13:25:10 +0200

    Laser.py get_functions()

--- omcu/submodules/Laser.py
+++ omcu/submodules/Laser.py
@@ -135,19 +135,14 @@ class Laser:
         """
         ld_string = self.__write_serial('ld?')  # returns string 'pulsed laser emission: off/on'
         print(ld_string)
-        #ld_val = 2  # global variable, place holder value
         if 'off' in ld_string[-3:]:
-            #global ld_val
             ld_val = 0  # global variable is redefined
-            return ld_val
         elif 'on' in ld_string[-3:]:
-            #global ld_val
             ld_val = 1  # global variable is redefined
-            return ld_val
         else:
             ld_val = 2
             print('Error: pulsed laser emission state could not be determined. Try again!')
-            return ld_val
+        return ld_val
 
     def set_trig_edge(self, te):
         """
