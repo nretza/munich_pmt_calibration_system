0d6538ba7927ec0c775247ddc12b49ef888c43fa
Laura <evalaura.winter@tum.de>
2021-05-04 16:48:31 +0200

    Powermeter.py set_echo(), get_echo() with __write_serial()

--- omcu/submodules/Powermeter.py
+++ omcu/submodules/Powermeter.py
@@ -63,7 +63,7 @@ class Powermeter:
         self.logger.debug(f'Serial write cmd: {cmd}; return {return_str}')
         return return_str
 
-    def set_echo(self, state):
+    def set_echo(self, state):  #TODO: use __write_serial()
         """
         This is a function to turn on or off the echoing of commands sent to the power meter.
         When the echo mode is enabled, the power meter generates a '>' prompt for every new line and all characters
@@ -73,22 +73,22 @@ class Powermeter:
         :param state: echo set (0 Echo OFF, 1 Echo ON)
         :return: echo status
         """
-        self.__write_serial(f'echo {state}', line_ending=b'\n')
-        # self.serial.write(str.encode('ECHO %s\r\n' % state))  # 0 Echo OFF, 1 Echo ON
-        # time.sleep(.5)
-        return self.get_echo()
+        #self.__write_serial(f'echo {state}', line_ending=b'\n')
+        self.serial.write(str.encode('ECHO %s\r\n' % state))  # 0 Echo OFF, 1 Echo ON
+        time.sleep(.5)
+        self.get_echo()
 
-    def get_echo(self):
+    def get_echo(self):  #TODO: use __write_serial()
         """
         This is a function to get information about the echo set
         :return: 0 Echo OFF, 1 Echo ON
         """
-        #self.serial.write(b'ECHO?\r\n')  # returns the status of the echo
-        # time.sleep(.5)
-        # line = self.serial.readline()
-        # print("The Echo status is:", line.decode(), "(0 = Echo OFF, 1 = Echo ON)")
-        echo = self.__write_serial(f'echo?', line_ending=b'\n')
-        return echo
+        self.serial.write(b'ECHO?\r\n')  # returns the status of the echo
+        time.sleep(.5)
+        line = self.serial.readline()
+        print("The Echo status is:", line.decode(), "(0 = Echo OFF, 1 = Echo ON)")
+        #echo = self.__write_serial(f'echo?', line_ending=b'\n')
+        #return echo
 
     def set_lambda(self, lamb):
         """
