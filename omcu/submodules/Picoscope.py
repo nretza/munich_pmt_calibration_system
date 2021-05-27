7e394e179d8afbfc701f71b11f6d70fd39acc582
Laura <evalaura.winter@tum.de>
2021-05-27 17:41:48 +0200

    Picoscope.py minor changes

--- omcu/submodules/Picoscope.py
+++ omcu/submodules/Picoscope.py
@@ -6,14 +6,12 @@ from picosdk.PicoDeviceEnums import picoEnum as enums
 import matplotlib.pyplot as plt
 from picosdk.functions import adc2mV, assert_pico_ok
 
-# Create chandle and status ready for use
 chandle = ctypes.c_int16()
 resolution = enums.PICO_DEVICE_RESOLUTION["PICO_DR_12BIT"]  # 12bit works for 2 channels, "PICO_DR_8BIT" for all 4
-status = {}
 
 class Picoscope:
+
     def __init__(self):
         self.chandle = chandle
         self.resolution = resolution
-        status["openunit"] = ps.ps6000aOpenUnit(ctypes.byref(self.chandle), None, self.resolution)
-        assert_pico_ok(status["openunit"])
+        ps.ps6000aOpenUnit(ctypes.byref(self.chandle), None, self.resolution)
