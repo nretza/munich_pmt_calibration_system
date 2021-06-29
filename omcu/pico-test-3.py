b54b031a8a3265df0d4ca6beb4e3ac22a6b9dce0
Laura <evalaura.winter@tum.de>
2021-06-29 14:43:05 +0200

    pico-test-3.py changes

--- omcu/pico-test-3.py
+++ omcu/pico-test-3.py
@@ -14,13 +14,11 @@ from picosdk.functions import adc2mV, assert_pico_ok, splitMSODataFast
 
 # Create chandle and status ready for use
 chandle = ctypes.c_int16()
-status = {}
 
 # Open 6000 A series PicoScope
 # returns handle to chandle for use in future API functions
 resolution = enums.PICO_DEVICE_RESOLUTION["PICO_DR_8BIT"]
-status["openunit"] = ps.ps6000aOpenUnit(ctypes.byref(chandle), None, resolution)
-assert_pico_ok(status["openunit"])
+ps.ps6000aOpenUnit(ctypes.byref(chandle), None, resolution)
 
 # Set channel A on
 # handle = chandle
@@ -29,14 +27,12 @@ coupling = enums.PICO_COUPLING["PICO_DC"]
 channelRange = 7
 # analogueOffset = 0 V
 bandwidth = enums.PICO_BANDWIDTH_LIMITER["PICO_BW_FULL"]
-status["setChannelA"] = ps.ps6000aSetChannelOn(chandle, channelA, coupling, channelRange, 0, bandwidth)
-assert_pico_ok(status["setChannelA"])
+ps.ps6000aSetChannelOn(chandle, channelA, coupling, channelRange, 0, bandwidth)
 
-# set channel B-H off
+# set channel B, C, D off
 for x in range(1, 3, 1):
     channel = x
-    status["setChannel", x] = ps.ps6000aSetChannelOff(chandle, channel)
-    assert_pico_ok(status["setChannel", x])
+    ps.ps6000aSetChannelOff(chandle, channel)
 
 # set MSO pod 1 on
 # handle = chandle
@@ -47,14 +43,12 @@ logicThresholdLevel = (ctypes.c_int16 * pins)(0)
 logicThresholdLevel[0] = 1000
 logicThresholdLevelLength = len(logicThresholdLevel)
 hysteresis = enums.PICO_DIGITAL_PORT_HYSTERESIS["PICO_LOW_50MV"]
-status["setDigitalPortOn"] = ps.ps6000aSetDigitalPortOn(chandle, port, ctypes.byref(logicThresholdLevel),
+ps.ps6000aSetDigitalPortOn(chandle, port, ctypes.byref(logicThresholdLevel),
                                                         logicThresholdLevelLength, hysteresis)
-assert_pico_ok(status["setDigitalPortOn"])
 
 # Set MSO pod 2 off
 port2 = enums.PICO_CHANNEL["PICO_PORT1"]
-status["setDigitalPortOff"] = ps.ps6000aSetDigitalPortOff(chandle, port2)
-assert_pico_ok(status["setDigitalPortOff"])
+ps.ps6000aSetDigitalPortOff(chandle, port2)
 
 # Set simple trigger on channel A, 1 V rising with 1 s autotrigger
 # handle = chandle
@@ -64,8 +58,7 @@ source = channelA
 direction = enums.PICO_THRESHOLD_DIRECTION["PICO_RISING"]
 # delay = 0 s
 # autoTriggerMicroSeconds = 1000000 us
-status["setSimpleTrigger"] = ps.ps6000aSetSimpleTrigger(chandle, 1, source, 1000, direction, 0, 1000000)
-assert_pico_ok(status["setSimpleTrigger"])
+ps.ps6000aSetSimpleTrigger(chandle, 1, source, 1000, direction, 0, 1000000)
 
 # Set number of samples to be collected
 noOfPreTriggerSamples = 500000
@@ -78,9 +71,8 @@ timebase = ctypes.c_uint32(1)
 timeInterval = ctypes.c_double(0)
 returnedMaxSamples = ctypes.c_uint64()
 # segment = 0
-status["getTimebase"] = ps.ps6000aGetTimebase(chandle, timebase, nSamples, ctypes.byref(timeInterval),
+ps.ps6000aGetTimebase(chandle, timebase, nSamples, ctypes.byref(timeInterval),
                                               ctypes.byref(returnedMaxSamples), 0)
-assert_pico_ok(status["getTimebase"])
 print("timebase = ", timebase.value)
 print("sample interval =", timeInterval.value, "ns")
 
@@ -103,15 +95,13 @@ downSampleMode = enums.PICO_RATIO_MODE["PICO_RATIO_MODE_RAW"]
 clear = enums.PICO_ACTION["PICO_CLEAR_ALL"]
 add = enums.PICO_ACTION["PICO_ADD"]
 action = clear | add  # PICO_ACTION["PICO_CLEAR_WAVEFORM_CLEAR_ALL"] | PICO_ACTION["PICO_ADD"]
-status["setDataChABuffers"] = ps.ps6000aSetDataBuffers(chandle, channelA, ctypes.byref(bufferAMax),
+ps.ps6000aSetDataBuffers(chandle, channelA, ctypes.byref(bufferAMax),
                                                        ctypes.byref(bufferAMin), nSamples, dataType, waveform,
                                                        downSampleMode, action)
-assert_pico_ok(status["setDataChABuffers"])
 
-status["setDataDP0Buffers"] = ps.ps6000aSetDataBuffers(chandle, port, ctypes.byref(bufferDPort0Max),
+ps.ps6000aSetDataBuffers(chandle, port, ctypes.byref(bufferDPort0Max),
                                                        ctypes.byref(bufferDPort0Min), nSamples, dataType, waveform,
                                                        downSampleMode, action)
-assert_pico_ok(status["setDataDP0Buffers"])
 
 # Run block capture
 # handle = chandle
@@ -120,15 +110,14 @@ timeIndisposedMs = ctypes.c_double(0)
 # segmentIndex = 0
 # lpReady = None   Using IsReady rather than a callback
 # pParameter = None
-status["runBlock"] = ps.ps6000aRunBlock(chandle, noOfPreTriggerSamples, noOfPostTriggerSamples, timebase,
+ps.ps6000aRunBlock(chandle, noOfPreTriggerSamples, noOfPostTriggerSamples, timebase,
                                         ctypes.byref(timeIndisposedMs), 0, None, None)
-assert_pico_ok(status["runBlock"])
 
 # Check for data collection to finish using ps5000aIsReady
 ready = ctypes.c_int16(0)
 check = ctypes.c_int16(0)
 while ready.value == check.value:
-    status["isReady"] = ps.ps6000aIsReady(chandle, ctypes.byref(ready))
+    ps.ps6000aIsReady(chandle, ctypes.byref(ready))
 
 # Get data from scope
 # handle = chandle
@@ -137,16 +126,14 @@ noOfSamples = ctypes.c_uint64(nSamples)
 # downSampleRatio = 1
 # segmentIndex = 0
 overflow = ctypes.c_int16(0)
-status["getValues"] = ps.ps6000aGetValues(chandle, 0, ctypes.byref(noOfSamples), 1, downSampleMode, 0,
+ps.ps6000aGetValues(chandle, 0, ctypes.byref(noOfSamples), 1, downSampleMode, 0,
                                           ctypes.byref(overflow))
-assert_pico_ok(status["getValues"])
 
 # get max ADC value
 # handle = chandle
 minADC = ctypes.c_int16()
 maxADC = ctypes.c_int16()
-status["getAdcLimits"] = ps.ps6000aGetAdcLimits(chandle, resolution, ctypes.byref(minADC), ctypes.byref(maxADC))
-assert_pico_ok(status["getAdcLimits"])
+ps.ps6000aGetAdcLimits(chandle, resolution, ctypes.byref(minADC), ctypes.byref(maxADC))
 
 # convert ADC counts data to mV
 adc2mVChAMax = adc2mV(bufferAMax, channelRange, maxADC)
@@ -183,7 +170,4 @@ plt.legend(loc="upper right")
 plt.show()
 
 # Close the scope
-status["closeunit"] = ps.ps6000aCloseUnit(chandle)
-assert_pico_ok(status["closeunit"])
-
-print(status)
+ps.ps6000aCloseUnit(chandle)
