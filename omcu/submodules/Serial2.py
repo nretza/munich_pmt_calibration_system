152eaeb114fe0a4877c23a4abd0c8b4f031f72cd
ChristopherFink <ga84qal@mytum.de>
2021-07-19 13:44:52 +0200

    changes in the rotation stage code

--- omcu/submodules/Serial2.py
+++ omcu/submodules/Serial2.py
@@ -10,13 +10,26 @@ def io_serial(cmd, delay=0.1,serial=serial):
         cmd: bytes
         delay: float or None
         """
+        sep = ': '
         if serial.in_waiting:
-            print(serial.read_until(size=serial.in_waiting))
+            string = str(serial.read_until(size=serial.in_waiting)[:-2], 'utf-8')
+            #split_str = string.split(sep)
+            #if split_str[0] == '0':
+            print(string, 'Leftover from the serial buffer!')
+            #print(str(serial.read_until(size=serial.in_waiting)[:-2], 'utf-8'))
         
         serial.write(cmd)
         
         while True:
             if serial.in_waiting > 0:
-                print(serial.read(size=serial.in_waiting)[:-2])
+                string = str(serial.read(size=serial.in_waiting)[:-2], 'utf-8')
+                split_str = string.split(sep)
+                if split_str[0] == '0':
+                    ret = float(split_str[1])
+                else:
+                    print(split_str[1])
+                #print(string)
+                
                 break
             time.sleep(delay)
+        return ret
--- omcu/submodules/rot_stage.py
+++ omcu/submodules/rot_stage.py
@@ -15,12 +15,81 @@ class Rotation_stage:
 
         # initialise Serial or SimSerial
         self.serial = serial_connection(dev,
-                                        baudrate=9600, #TODO: change baudrate back
+                                        baudrate=9600, #TODO: change baudrate back, this is currently in the ardunio code!
                                         bytesize=serial.EIGHTBITS,
                                         parity=serial.PARITY_NONE,
                                         timeout=2
                                         )
         
+    def go_phi(self,phi):
+        """
+        Controls the upper stepper. Phi direction is relativ to the PMT and not a global coordinate system
+
+        PARAMETERS
+        ----------
+        phi: Float
+        """
+        
+        ret = io_serial(bytes(f'goY {phi}', 'utf-8'), serial=self.serial)
+        return ret
+        
+        
+    def go_theta(self,theta):
+        """
+        Controls the lower stepper. Phi direction is relativ to the PMT and not a global coordinate system
+
+        PARAMETERS
+        ----------
+        phi: Float
+        """
+        return io_serial(bytes(f'goX {theta}', 'utf-8'), serial=self.serial)
+        
+    def go_home(self):
+        """
+        sends gohome command
+        """
+        ret = [0,0]
+        ret[0] = io_serial(bytes(f'goHomeY', 'utf-8'), serial=self.serial)
+        ret[1] = io_serial(bytes(f'goHomeX', 'utf-8'), serial=self.serial)
+        return ret
+    
+    def set_speed(self,speed):
+        """
+        sets the delaytime inbetween single steps in milliseconds. Minimum value is 300.
+        PARAMETERS
+        ----------
+        speed: Float
+        """
+        return io_serial(bytes(f'setspeed {speed}', 'utf-8'), serial=self.serial)
+        
+    def scan(self,phi,theta):
+        """
+        Gives the rotation stage 2D coordinates to move to. First phi, than theta direction
+        PARAMETERS
+        ----------
+        phi: Float
+        theta: Float
+        """
+        ret = [0,0]
+        ret[0] = self.go_phi(phi)
+        ret[1] = self.go_theta(theta)
+        return ret
+    
+    def get_position(self):
+        """
+        Returns the current position
+        """
+        ret = [0,0]
+        ret[0] = io_serial(bytes(f'getPositionY', 'utf-8'), serial=self.serial)
+        ret[1] = io_serial(bytes(f'getPositionX', 'utf-8'), serial=self.serial)
+        return ret
+        
+    def get_speed(self):
+        """
+        Returns the current position
+        """
+        return io_serial(bytes(f'getspeed', 'utf-8'), serial=self.serial)
+    
     '''def __write_serial(self, cmd, delay=None, line_ending=b'\r\n'):  # "__" : private function for this class### needed, why can't import?!
         """
 
@@ -71,65 +140,4 @@ class Rotation_stage:
             time.sleep(delay)
       '''  
         
-        
-    def go_phi(self,phi):
-        """
-        Controls the upper stepper. Phi direction is relativ to the PMT and not a global coordinate system
-
-        PARAMETERS
-        ----------
-        phi: Float
-        """
-        
-        io_serial(bytes(f'goY {phi}', 'utf-8'), serial=self.serial)
-        
-        
-    def go_theta(self,theta):
-        """
-        Controls the lower stepper. Phi direction is relativ to the PMT and not a global coordinate system
-
-        PARAMETERS
-        ----------
-        phi: Float
-        """
-        io_serial(bytes(f'goX {theta}', 'utf-8'), serial=self.serial)
-        
-    def go_home(self):
-        """
-        sends gohome command
-        """
-        io_serial(bytes(f'gohome', 'utf-8'), serial=self.serial)
-        time.sleep(0.5)
-        print(self.serial.read(size=self.serial.in_waiting)[:-2])
-        
-    def set_speed(self,speed):
-        """
-        sets the delaytime inbetween single steps in milliseconds. Minimum value is 300.
-        PARAMETERS
-        ----------
-        speed: Float
-        """
-        io_serial(bytes(f'setspeed {speed}', 'utf-8'), serial=self.serial)
-        
-    def scan(self,phi,theta):
-        """
-        Gives the Laser 2D coordinates to move to. First phi, than theta direction
-        PARAMETERS
-        ----------
-        phi: Float
-        theta: Float
-        """
-        self.go_phi(phi)
-        self.go_theta(theta)
-    
-    def get_position(self):
-        """
-        Returns the current position
-        """
-        io_serial(bytes(f'getPosition', 'utf-8'), serial=self.serial)
-        
-    def get_speed(self):
-        """
-        Returns the current position
-        """
-        io_serial(bytes(f'getspeed', 'utf-8'), serial=self.serial)
+     
