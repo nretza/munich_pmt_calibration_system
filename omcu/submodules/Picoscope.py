f6969285e39fcac657e04a1e9b4d3a837c26d2d4
Laura <evalaura.winter@tum.de>
2021-08-19 09:54:58 +0200

    Picoscope.py: changes get_offset_limits

--- omcu/submodules/Picoscope.py
+++ omcu/submodules/Picoscope.py
@@ -104,7 +104,7 @@ class Picoscope:
             ps.ps6000aSetChannelOn(self.chandle, ch, self.coupling, self.voltrange, 0, self.bandwidth)
 
     def get_offset_limits(self, voltrange=5, coupling=50):
-        ps.ps6000aGetAnalogueOffsetLimits(self.chandle, voltrange, coupling, 0, 0)
+        return ps.ps6000aGetAnalogueOffsetLimits(self.chandle, voltrange, coupling, 0, 0)
 
     def trigger_setup(self, channel=0, direction=2, threshold=1000):
         """
