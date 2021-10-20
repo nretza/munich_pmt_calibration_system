1746eb496104e266caa51bef8397ab367bf9b67b
Laura <evalaura.winter@tum.de>
2021-10-20 14:19:03 +0200

    omcu_meas2 : number_theta and number_phi in filename, Occupancy changes according to new data format

--- omcu/Occupancy.py
+++ omcu/Occupancy.py
@@ -7,7 +7,23 @@ class Occupancy:
     def __init__(self):
         pass
 
-    def occ(self, file='./data/20210823-112905-10000-sgnl.npy', threshold=-4):
+    def occ(self, filename='data/20211020-140122/20211020-140122-5-15-10.npy', threshold=-0.8):
+
+        data = np.load(filename)
+        filename_split1 = filename.split('/')  # ['data', '20211020-140122', '20211020-140122-5-15-10.npy']
+        filename_split2 = filename_split1[-1].split('-')  # ['20211020', '140122', '5', '15', '10.npy']
+        filename_split3 = filename_split2[-1].split('.')  # ['10', 'npy']
+        number = int(filename_split3[0])  # 10 = number of waveforms per measurement
+        delta_theta = int(filename_split2[2])  # 5
+        delta_phi = int(filename_split2[3])  # 15
+        total_number = int((90/delta_theta) * (360/delta_phi))  # 432 = number of measurements
+        total_number_waveforms = number * total_number
+        minval = np.zeros(total_number_waveforms)
+        #for i in range():
+
+
+
+    def occ_old(self, file='./data/20210823-112905-10000-sgnl.npy', threshold=-4):
 
         data = np.load(file)
         number = len(data)
--- omcu/omcu-meas2.py
+++ omcu/omcu-meas2.py
@@ -65,7 +65,7 @@ for i, theta in enumerate(range(0, 90, delta_theta)):  # rotation in xy plane
         data[i][j][1] = data_trg
         time.sleep(0.1)
 
-filename_Ps = directory + '/' + timestr + '-' + str(delta_theta) + '-' + str(delta_phi) + '-' + str(number) + '.npy'
+filename_Ps = directory + '/' + timestr + '-' + str(number_theta) + '-' + str(number_phi) + '-' + str(number) + '.npy'
 np.save(filename_Ps, data)
 filename_Pm = directory + '/' + timestr + '-Powermeter-data.npy'
 
