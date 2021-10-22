507fb9036dfde29f6d2fef93b14c7fb774236b52
Laura <evalaura.winter@tum.de>
2021-10-22 14:53:37 +0200

    Plots_and_histograms.py added plot_histogram_charge and try

--- omcu/Occupancy.py
+++ omcu/Occupancy.py
@@ -10,14 +10,14 @@ class Occupancy:
     def occ(self, filename='data/20211022-140412-1.1/20211022-140412-6-4-10.npy', threshold=-0.8):
 
         data = np.load(filename)
-        filename_split1 = filename.split('/')  # ['data', '20211020-143911', '20211020-143911-3-4-10.npy']
-        filename_split2 = filename_split1[-1].split('-')  # ['20211020', '143911', '3', '4', '10.npy']
+        filename_split1 = filename.split('/')  # ['data', '20211022-140412-1.1', '20211022-140412-6-4-10.npy']
+        filename_split2 = filename_split1[-1].split('-')  # ['20211020', '140412', '6', '4', '10.npy']
         filename_split3 = filename_split2[-1].split('.')  # ['10', 'npy']
         number = int(filename_split3[0])  # 10 = number of waveforms per measurement
-        number_theta = int(filename_split2[2])  # 3
+        number_theta = int(filename_split2[2])  # 6
         number_phi = int(filename_split2[3])  # 4
-        total_number = int(number_theta * number_phi)  # 12 = number of measurements
-        total_number_waveforms = number * total_number  # 120
+        total_number = int(number_theta * number_phi)  # 24 = number of measurements
+        total_number_waveforms = number * total_number  # 240
         minval = np.zeros(total_number_waveforms)
         m=0
         for i in range(0, number_theta, 1):
--- omcu/Plots_and_histograms.py
+++ omcu/Plots_and_histograms.py
@@ -15,23 +15,29 @@ class Plots:
         Ps = Picoscope()
         self.nSamples = Ps.get_nSamples()
 
-    def plot_waveforms(self, filename='data/20211022-140412-1.1/20211022-140412-6-4-10.npy'):
+    def get_info(self, filename='data/20211022-140412-1.1/20211022-140412-6-4-10.npy'):
 
-        data = np.load(filename)
         filename_split1 = filename.split('/')  # ['data', '20211022-140412-1.1', '20211022-140412-6-4-10.npy']
+        folder_name = filename_split1[1]
         filename_split2 = filename_split1[-1].split('-')  # ['20211020', '140412', '6', '4', '10.npy']
         filename_split3 = filename_split2[-1].split('.')  # ['10', 'npy']
         number = int(filename_split3[0])  # 10 = number of waveforms per measurement
-        number_theta = int(filename_split2[2])  # 3
+        number_theta = int(filename_split2[2])  # 6
         number_phi = int(filename_split2[3])  # 4
-        total_number = int(number_theta * number_phi)  # 12 = number of measurements
-        total_number_waveforms = number * total_number  # 120
-        nSamples = self.nSamples
+        total_number = int(number_theta * number_phi)  # 24 = number of measurements
+        total_number_waveforms = number * total_number  # 240
 
-        directory = 'data/plots/' + filename_split1[1]
+        return folder_name, number, number_theta, number_phi, total_number, total_number_waveforms
+
+    def plot_waveforms(self, filename='data/20211022-140412-1.1/20211022-140412-6-4-10.npy'):
+
+        data = np.load(filename)
+        folder_name, number, number_theta, number_phi, total_number, total_number_waveforms = self.get_info(filename)
+
+        directory = 'data/plots/' + folder_name
         if not os.path.exists(directory):
             os.mkdir(directory)
-        figname = directory + '/' + filename_split1[1] + '-' + str(number_theta) + '-' + str(number_phi) + '-'\
+        figname = directory + '/' + folder_name + '-' + str(number_theta) + '-' + str(number_phi) + '-'\
                   + str(number) + '-Figure.pdf'
 
         plt.figure()
@@ -46,3 +52,23 @@ class Plots:
 
         plt.savefig(figname)
         plt.show()
+
+    def plot_histogram_charge(self, filename='data/20211022-140412-1.1/20211022-140412-6-4-10.npy', nBins=20):
+
+        data = np.load(filename)
+        folder_name, number, number_theta, number_phi, total_number, total_number_waveforms = self.get_info(filename)
+
+        directory = 'data/plots/' + folder_name
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+        figname = directory + '/' + folder_name + '-' + str(number_theta) + '-' + str(number_phi) + '-' \
+                  + str(number) + '-Histogram.pdf'
+
+        nSamples = self.nSamples
+
+        for i, n in enumerate(data):
+            for j, m in enumerate(n):
+                for k in m[0]:
+                    x = k[:, 0]
+                    y = k[:, 1]
+                    print('i=', i, 'j=', j, x)
