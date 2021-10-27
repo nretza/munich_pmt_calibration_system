df45c4daf0fe344cfa00b288cf3dbbeb3c0b4bd4
Laura <evalaura.winter@tum.de>
2021-10-27 18:11:02 +0200

    Data_analysis.py : started

--- omcu/Data_analysis.py
+++ omcu/Data_analysis.py
@@ -7,3 +7,16 @@ import numpy as np
 from numpy import trapz
 import scipy.constants as const
 import h5py
+
+class Analysis:
+
+    def __init__(self):
+
+        Ps = Picoscope()
+        self.nSamples = Ps.get_nSamples()
+
+    def filter_sph_and_set_data_to_SI(self, filename='./data/PMT001/test_with_attr'):
+
+        with h5py.File(filename, 'r') as f:
+            for arr in f:  #TODO: add
+                pass
