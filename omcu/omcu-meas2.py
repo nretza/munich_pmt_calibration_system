f41375b6873ca14000e0d17143de60b1f947817f
canada <canada@straw.com>
2021-10-27 16:51:08 +0200

    Lab changes

--- omcu/omcu-meas2.py
+++ omcu/omcu-meas2.py
@@ -38,6 +38,12 @@ time.sleep(300)  # wait 5 minutes so that it's dark
 Pm.set_offset()  # set offset value when it's dark
 #Pa_data_dark = Pa.read_ch1(100)  # value? data collection of Picoamp
 
+# turn on laser for it to heat up
+L.on_pulsed()  # pulsed laser emission on
+time.sleep(1800)  # wait 30 minutes
+power0 = Pm.get_power()
+print('Powermeter:', power0, 'W')
+
 # Psu settings
 Psu0.settings(1, voltage=12.0, current=3.0)  # psu for rotation stage
 Psu1.settings(1, voltage=5.0, current=0.1)  # psu for PMT, Vcc
@@ -52,7 +58,6 @@ f0 = 10e3
 L.set_freq(f=f0)  # value?
 tune = 710
 L.set_tune_value(tune=tune)  # value?
-L.on_pulsed()  # pulsed laser emission on
 number0 = 10000
 data_sgnl, data_trg = Ps.block_measurement(trgchannel=0, sgnlchannel=2, direction=2, threshold=2000, number=number0)
 occ = oc.occ_data(data_sgnl, -4)
@@ -80,16 +85,16 @@ for i, theta in enumerate(thetas):  # rotation in xy plane
         time.sleep(0.1)
         power = Pm.get_power()
         # Pa_data = Pa.read_ch1(100)  # value? data collection of Picoamp
-        folder_sgnl = str(theta) + '/' + str(phi) + '/signal'
-        folder_trg = str(theta) + '/' + str(phi) + '/trigger'
-        arr_sgnl = h5.create_dataset(folder_sgnl, (number, nSamples, 2), dtype='f')
+        folder_sgnl = '/' + str(theta) + '/' + str(phi) + '/signal'
+        folder_trg = '/' + str(theta) + '/' + str(phi) + '/trigger'
+        arr_sgnl = h5.create_dataset(folder_sgnl, (number, nSamples, 2), dtype='f')  #TODO: ValueError: Invalid location identifier (invalid location identifier)
         arr_trg = h5.create_dataset(folder_trg, (number, nSamples, 2), dtype='f')
         arr_sgnl[:], arr_trg[:] = Ps.block_measurement(trgchannel=0, sgnlchannel=2, direction=2, threshold=2000,
                                                    number=number)
-        arr_sgnl.attrs['Vctrl'], arr_trg.attrs['Vctrl'] = Vctrl
-        arr_sgnl.attrs['Powermeter'], arr_trg.attrs['Powermeter'] = power
-        arr_sgnl.attrs['Units_voltage'], arr_trg.attrs['Units_voltage'] = 'mV'
-        arr_sgnl.attrs['Units_time'], arr_trg.attrs['Units_time'] = 'ns'
+        #arr_sgnl.attrs['Vctrl'], arr_trg.attrs['Vctrl'] = Vctrl
+        #arr_sgnl.attrs['Powermeter'], arr_trg.attrs['Powermeter'] = power
+        #arr_sgnl.attrs['Units_voltage'], arr_trg.attrs['Units_voltage'] = 'mV'
+        #arr_sgnl.attrs['Units_time'], arr_trg.attrs['Units_time'] = 'ns'
         #TODO: mehr Attribute?
         h5.close()
         time.sleep(0.1)
